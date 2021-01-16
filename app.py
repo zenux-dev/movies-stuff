73ef400598da30b3232d5a86c0b0f0ad2667d4dd
Zenux <77491495+zenux-dev@users.noreply.github.com>
2021-01-16 17:04:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def download():
 		return redirect(download_link+"&dl=1")
 
 if __name__ == '__main__':
-	app.run(debug=False,host='0.0.0.0',port="5000")
+	app.run(debug=False,host='0.0.0.0',port="4444")
